a66dcd59bc0a99d3485e285708e48582479675fe
Ahmed Sadik <ahsadik@student.1337.ma>
2026-04-14 09:48:12 +0100

    feat: implement chat endpoints and enhance embedding functions

--- backend/api/chat.py
+++ backend/api/chat.py
@@ -1,5 +1,39 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Form
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from services.embedding import embed_question
+from services.embedding import get_relevant_chunks
+from services.database import get_chunks_by_ids
+
+class ChatRequest(BaseModel):
+	question: str
+	uuid: str
 
 route = APIRouter(prefix="/chat", tags=["Chat"])
 
-#TODO : implement chat endpoints
+@route.get("/ask")
+async def ask():
+	return HTMLResponse(content="""
+					 <html><body>
+					 <h1>Chat</h1>
+					 <p>Welcome to the chat!</p>
+					 <form action="/chat/ask" method="post">
+					 <input type="hidden" name="uuid" value="3fff2a461be5e01615f8893012730408">
+					 <input type="text" name="question" placeholder="Ask a question..." required>
+					 <button type="submit">Send</button>
+					 </form>
+					 </body></html>
+					 """)
+
+@route.post("/ask")
+async def ask_question(request: ChatRequest = Form(...)):
+	question = embed_question(request.question)
+	chunks_ids = get_relevant_chunks(question, request.uuid)
+	try:
+		chunks = get_chunks_by_ids(chunks_ids)
+	except Exception as e:
+		print(f"Error retrieving chunks: {e}")
+		raise
+	debug = f"Question: {request.question}\nRelevant Chunks:\n" + "<br><br>".join(chunks)
+
+	return HTMLResponse(content=f"<html><body><h1>Chat</h1><p>{debug}</p><a href='/chat/ask'>Ask another question</a></body></html>")
--- backend/services/embedding.py
+++ backend/services/embedding.py
@@ -1,7 +1,6 @@
 import faiss
-from sentence_transformers import SentenceTransformer
 import numpy as np
-
+from services.model_loader import get_model
 from config import FAISS_DIR
 
 def save_embeddings(embeddings, chunks_ids, uuid):
@@ -13,9 +12,19 @@ def save_embeddings(embeddings, chunks_ids, uuid):
 
 def generate_embeddings(chunks):
 	# Load the pre-trained model
-	model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+	model = get_model()
 
 	# Generate embeddings for each chunk of text
 	embeddings = model.encode(chunks)
 
 	return embeddings
+
+def embed_question(question):
+	model = get_model()
+	embedding = model.encode([question])
+	return embedding
+
+def get_relevant_chunks(question_embedding, uuid, top_k=5):
+	index = faiss.read_index(str(FAISS_DIR / f"{uuid}.faiss"))
+	distances, indices = index.search(question_embedding, top_k)
+	return indices[0].tolist()  # Return the list of relevant chunk IDs
