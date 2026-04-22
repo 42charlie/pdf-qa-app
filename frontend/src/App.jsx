13b9381718a4f9d9dab25163a90eb005e966b93a
Ahmed Sadik <ahsadik@student.1337.ma>
2026-04-22 13:51:19 +0100

    feat: implement initial layout for App component with left and right panels

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,15 @@
 
+function App() {
+  return (
+    <div className="flex">
+      <div id='left-panel' className="bg-gray-500 flex-[1.2]">
+        <div className="min-h-screen">EXPLANATION HERE</div>
+        <div className="min-h-screen">EXPLANATION HERE</div>
+        <div className="min-h-screen">EXPLANATION HERE</div>
+      </div>
+      <div id='right-panel' className="sticky top-0 bg-gray-300 flex-1 h-screen">VISUALIZER HERE</div>
+    </div>
+  );
+}
+
+export default App;
