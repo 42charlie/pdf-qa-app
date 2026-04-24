282531884c4c3d420f9157076bec1b62137ff086
Ahmed Sadik <breaksadik@gmail.com>
2026-04-24 19:05:51 +0100

    feat: add react-icons for social links in Banner component

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,6 +1,19 @@
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+
 function Banner() {
   return (
-	<div className="w-14 bg-slate-50">
+	<div className="w-14 bg-slate-50 flex-col py-4 mt-4">
+		<div className="space-y-4 text-xl text-slate-400 flex flex-col items-center">
+			<a href="https://github.com/42charlie/pdf-qa-app" target="_blank" rel="noopener noreferrer" className="p-2 hover:text-slate-900 transition hover:bg-slate-200 rounded-md">
+				<FaGithub className="m-auto"/>
+			</a>
+			<a href="https://linkedin.com/42charlie" target="_blank" rel="noopener noreferrer" className="p-2 hover:text-sky-700 transition hover:bg-blue-50 rounded-md">
+				<FaLinkedin className="m-auto"/>
+			</a>
+			<a href="mailto:breaksadik@gmail.com" target="_blank" rel="noopener noreferrer" className="p-2 hover:text-red-500 transition hover:bg-red-50 rounded-md">
+				<FaEnvelope className="m-auto"/>
+			</a>
+		</div>
 	</div>
   );
 }
